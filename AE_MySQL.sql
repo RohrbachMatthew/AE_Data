21b66756d093253ded4ef5ba730e9acf6c966b87
RohrbachMatthew <matthewrohrbach1091@gmail.com>
2025-07-13 01:53:11 -0400

    initial

---
+++ AE_MySQL.sql
@@ -0,0 +1,17 @@
+-- Create the table
+/*
+create table ae_data(
+ae_date date,
+total_parcel_count int,
+total_payout_amount decimal(19,18),
+ae_time time
+) */
+
+-- -----------------------------------
+
+-- EXAMPLE of one entry at a time
+/*
+insert into ae_data(ae_date, total_parcel_count, total_payout_amount, ae_time)
+values('2025-07-12', 43, 1.347425943461399652, '19:08:00')
+*/
+
